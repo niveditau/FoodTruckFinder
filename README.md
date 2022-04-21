ed8dca3ed53fe47218cfa12306e2ef8852905372
Nivedita Upadhyay <niveditaupadhyay@hotmail.com>
2022-04-21 13:15:30 -0700

    Improved tests statements

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # FoodTruckFinder
-This is a node.js app to get the food truck locations by querying geospatial database postgres postgis. For displaying user interface it uses .pug layout files. For displaying food truck locations on the map it uses Open Street Maps. Express is used for handling HTTP requests. It uses Mocha Chai for unit and integration tests.
-This project uses three tier architecture. First layer is the frontend that calls the API layer and API layer calls the database layer to query for the data. Following diagram describes the architecture of the app.
-
-<img width="854" alt="Screen Shot 2021-06-06 at 6 28 40 PM" src="https://user-images.githubusercontent.com/2191681/120948466-1bc43f80-c6f7-11eb-9be1-6647546a37eb.png">
-
-
-Supported queries:
-1. User can query food truck locations by longitude, latitude and radius. Query will return the food truck locations within the given radius from the longitude and latitude.
-2. User can query food truck locations by name. Query will return the food truck locations with matching name(applicant).
-3. User can query food truck locations by food item. Query will return the food truck locations by matching food item.
--- pg_mapper/index.test.js
+++ pg_mapper/index.test.js
@@ -5,18 +5,18 @@ var myIndex = require('./routes/index');
 var databaseQueryHandler = require('./routes/database/dbQueryHandler');
 const assert = require('assert').strict;
 
-describe("Unit test", function() {
+describe("Tests for food truck finder", function() {
     after(function() {
         databaseQueryHandler.endDatabaseConnection();
     });
-    it("paramter checking", function() {
+    it("should fail for null parameters", function() {
         const expectedError = new Error("parameters long/lat/radius cannot be null.");
 
         databaseQueryHandler.findFoodTrucksByCoordinates(null, null, null).catch(error => {
             assert.notStrictEqual(error, expectedError);
         });
     });
-    it("Should return food truck locations", async function() {
+    it("Should return food truck locations for given longitude, latitude and radius", async function() {
         var response = await databaseQueryHandler.findFoodTrucksByCoordinates(-122.391265350599, 37.7638112772906, 1000);
         assert.strictEqual(response.features.length, 51);
     });
@@ -24,11 +24,11 @@ describe("Unit test", function() {
         var response = await databaseQueryHandler.findFoodTrucksByCoordinates(-1, 3, 1000);
         assert.strictEqual(response.features, null);
     });
-    it("Should return 7 locations for non existing longiude and latitude",  async function() {
+    it("Should return 7 locations for applicant name 'grill'",  async function() {
         var response =  await databaseQueryHandler.findFoodTrucksByApplicant("grill");
         assert.strictEqual(response.features.length, 7);
     });
-    it("Should return 12 locations for non existing longiude and latitude",  async function() {
+    it("Should return 12 locations for food item 'pita'",  async function() {
         var response =  await databaseQueryHandler.findFoodTrucksByFoodItem("pita");
         assert.strictEqual(response.features.length, 12);
     });
